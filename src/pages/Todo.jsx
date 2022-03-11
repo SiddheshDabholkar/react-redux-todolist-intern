78127617588e9e3e8ae62140b17539320b832d8e
Siddhesh Dabholkar <iamsiddhesh22@gmail.com>
2022-03-11 15:38:09 +0530

    nav

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,6 +1,43 @@
-import React from "react";
+import React, { useReducer } from "react";
 import "./Todo.scss";
+import Input from "../components/Input/Input";
+import Button from "../components/button/Button";
+import useForm from "../hooks/useForm";
+import { useSelector, useDispatch } from "react-redux";
+import { addTodo, deleteTodo, editTodo } from "../redux/todo/todo.action";
+import Card from "../components/card/Card";
 
 export default function Todo() {
-  return <div>Todo</div>;
+  const { formData, handleInputChange } = useForm({
+    todo: "",
+  });
+  const todos = useSelector((state) => state.todo);
+  const user = useSelector((state) => state?.auth?.user);
+  const dispatch = useDispatch();
+
+  return (
+    <main className="Todo">
+      <nav className="TodoNav">
+        <div className="TodoNavLeft">
+          <span className="TodoNavLeftHead">Todo</span>
+        </div>
+        <div className="TodoNavRight">
+          <span className="TodoNavRightSpan">
+            logged in as <b>{user.name}</b>
+          </span>
+          <Button
+            p="10px"
+            br="10px"
+            bc="transparent"
+            c="pointer"
+            bg="red"
+            color="#fff"
+          >
+            Logout
+          </Button>
+        </div>
+      </nav>
+      <section></section>
+    </main>
+  );
 }
